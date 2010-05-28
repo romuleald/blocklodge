cd8397e3b3edc13ccbf28e7fe5e15ac47fefda5c
ckkoshi <ckkoshi@35d35e36-d370-634c-23f7-7196f0c5802a>
2010-05-28 23:42:11 +0000

    first code base - jquery based

---
+++ website/medias/js/script.js
@@ -0,0 +1,20 @@
+/* jquery 1.4.2 based */
+$().ready(function()
+{
+	BL.sizeStructure();
+});
+$(window).resize(function()
+{
+	BL.sizeStructure();
+});
+
+/* nameSpace BL */
+
+var BL = {};
+
+BL.sizeStructure = function()
+{
+	iHeight = document.body.clientHeight - document.getElementById('top').clientHeight -2;
+	console.info(iHeight);
+	$('#forum,#core,#frienBar').height(iHeight);
+};
