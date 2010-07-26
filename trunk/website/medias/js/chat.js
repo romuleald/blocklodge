ad3e714f31af80db91979429fa00c89e5caaaacd
ckkoshi <ckkoshi@35d35e36-d370-634c-23f7-7196f0c5802a>
2010-07-26 23:08:27 +0000

    * order by last * scroll vers la fin sauf si on a scrollé le chat (pour éviter d'être remonté tout le temps si on veut relire les logs * enter to post * 2 caractères minimum * supression des <tag> html + protection des \ et ' * parties non dev caché * début de structure JSON pour le refresh des éléments seulement nouveau *

--- trunk/website/medias/js/chat.js
+++ trunk/website/medias/js/chat.js
@@ -11,8 +11,8 @@ l'user arrive
 ** peuple les derniers chats
 * refresh du chat toute les xx ms
 * l'user post un message
-** stocké en base
-** une fois stocké, force du refresh du chat
+** stockÃ© en base
+** une fois stockÃ©, force du refresh du chat
 
 */
 
@@ -29,39 +29,87 @@ BL.chat = {
 		BL.chat.obj.JQoForm = $('#chatForm');
 		BL.chat.obj.JQoChats = $('#chat');
 		BL.chat.obj.JQoChatList = $('#chatList');
-		$('#chatForm').submit(function(e){
-			e.stopPropagation();
-			//console.info($(this).find('textarea')[0].value)
-			BL.chat.sendChat($(this).serialize())
+
+		$('#chatForm').submit(function(e)
+		{
+			if($('#chatMsg')[0].textLength > 1) {
+				//console.info(this);
+				e.stopPropagation();
+				BL.chat.sendChat($(this).serialize())
+			}
 		});
-		BL.chat.refreshView();
+		$('#chatMsg').keydown(function(e)
+		{
+			if(e.keyCode == 13)
+			{
+				BL.chat.obj.JQoForm.trigger('submit');
+				//console.info(this);
+				e.preventDefault();
+			}
+		});
+
+		BL.chat.obj.JQoChatList.scroll(function()
+		{
+			if(BL.chat.obj.JQoChatList[0].scrollTop + BL.chat.obj.JQoChatList.height() == BL.chat.obj.JQoChatList[0].scrollHeight)
+			{
+				BL.chat.obj.bCanScroll = true;
+			}
+			else
+			{
+				BL.chat.obj.bCanScroll = false;
+			}
+
+		});
+		BL.chat.refreshView(true);
+
 	},
 	sendChat:function(sDataToSent){
-		console.info(sDataToSent);
+		//console.info(sDataToSent);
 		$.ajax({
 			url:"chat.php",
 			type:"POST",
 			data:sDataToSent,
-			success:function(data){
-				console.info('data', data);
-				BL.chat.refreshView();
+			success:function(){
+				//console.info('data', data);
+				$('#chatMsg')[0].value = '';
+				BL.chat.refreshView(false);
 			}
 		})
+
 	},
-	refreshView:function(){
+	refreshView:function(bIsFirst){
 		clearTimeout(BL.chat.obj.timeOut);
-		console.info('refresh');
+		//console.info('refresh');
+		var sUrl = (bIsFirst) ? 'chat.php?html=true' : 'chat.php?html=true';
 		$.ajax({
-			url:"chat.php",
+			url:sUrl,
 			success:function(data){
-				BL.chat.buildView(data);
+				BL.chat.buildView(data, bIsFirst);
+				BL.chat.scrollTo();
+				BL.chat.obj.timeOut = setTimeout(function(){BL.chat.refreshView(false)},1500);
 			}
 		});
-		BL.chat.obj.timeOut = setTimeout(BL.chat.refreshView,1000);
+
 	},
-	buildView:function(data){
+	buildView:function(data, bIsHtml){
 		//actually html is build by php, after, php will send json and html will be build here
-		BL.chat.obj.JQoChatList.html(data);
+		if(bIsHtml){
+    	BL.chat.obj.JQoChatList.html(data);
+		}
+		else
+		{
+			BL.chat.obj.JQoChatList.html(data);
+			//console.info(eval(data));
+		}
+
+
+	},
+	scrollTo:function(){
+		if(BL.chat.obj.bCanScroll)
+		{
+	   	BL.chat.obj.JQoChatList.find("div:last")[0].scrollIntoView();
+		}
+
 	}
 
 };
