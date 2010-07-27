124daec2d3db3404258bf2fe95aecbf3d505c195
ckkoshi <ckkoshi@35d35e36-d370-634c-23f7-7196f0c5802a>
2010-07-27 21:22:04 +0000

    (Fixes issue 7) * remplacement du innerHTML par une injection DOM * limitation SQL pour ne remonter que les nouveau éléments (un where du lastId)
    UI
    * ajout typo google Nobile

--- trunk/website/chat.php
+++ trunk/website/chat.php
@@ -1,42 +1,50 @@
 <?php
+
 	include "inc/tehbd.php";
-	$link = mysql_connect('localhost', $sUsr, $sPsswrd);
+	$link = mysql_connect('127.0.0.1', $sUsr, $sPsswrd);
 	$db = mysql_select_db($sDbName, $link);
-//	$link = mysql_connect("localhost", "romuald", "chance") or die('ouin');
-//	$db = mysql_select_db('frigo', $link);
+//	$link = mysql_connect("127.0.0.1", "root", "");
+//	$db = mysql_select_db('blacklodge', $link);
 if(isset($_POST['id']))
 {
-	?>!!!<?php
 	$sQuery = "INSERT INTO  `chat` (  `user` ,  `id` ,  `post` ,  `date` ) VALUES ('". $_POST['user'] . "', '" . $_POST['id'] . "', '" . strip_tags(addslashes($_POST['post'])) . "', CURRENT_TIMESTAMP)";
 	mysql_query($sQuery);
 }
 else{
-	$sQuery = mysql_query("SELECT * FROM  `chat` ORDER BY  `index`");
+	$sQuery = mysql_query("SELECT * FROM `chat` WHERE `index` > ".$_GET['lastid']." ORDER BY `index`");
 
-	while($row = mysql_fetch_assoc($sQuery)){
 	?>
-	<?php if(isset($_GET['html'])) { ?>
-	<div class="padding" id="chat<?php echo $row["index"];?>">
-		<p>
-			<?php echo $row["user"];?>
-			<?php echo $row["id"];?>
-		</p>
-		<p><?php echo $row["post"];?></p>
-		<p>à : <?php echo $row["date"];?></p>
-		<hr/>
-	</div>
-	<?php } else { header('Content-type: text/javascript') ?>
-	[{
-		index:<?php echo $row["index"];?>,
-		user:<?php echo $row["user"];?>,
-		id:<?php echo $row["id"];?>,
-		post:<?php echo $row["post"];?>,
-		date:<?php echo $row["date"];?>
-	}]
-	<?php } ?>
-	<?php
-	}
-}
-mysql_close($link);
-
+<?php if(isset($_GET['html']))
+	{
+		while($row = mysql_fetch_assoc($sQuery)){
 ?>
+	<div class="padding" id="chat<?php echo $row["index"];?>">
+		<p class="user">
+			<?php echo $row["user"];?>
+			<?php //echo $row["id"];?>
+		</p>
+		<p class="post"><?php echo $row["post"];?></p>
+		<p class="date">à : <?php echo $row["date"];?></p>
+	</div>
+	<?php
+		}
+	}
+	else
+	{
+//		header('Content-type: text/javascript');
+		?>[<?php
+//			echo mysql_fetch_assoc($sQuery);
+			$index = 0;
+			while($row = mysql_fetch_assoc($sQuery))
+			{
+				$index++;
+				echo json_encode($row);
+				if(mysql_num_rows($sQuery) > $index)
+				{
+					echo ',';
+				}
+			}?>]<?php
+
+		 } ?>
+<?php } ?>
+<?php mysql_close($link); ?>
